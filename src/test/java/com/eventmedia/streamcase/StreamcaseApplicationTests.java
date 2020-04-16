dbac1f97f325424526dfc73db62d424b73f7be53
berlot83 <berlot83@yahoo.com.ar>
2020-04-15 23:05:40 -0300

    First commit

---
+++ src/test/java/com/eventmedia/streamcase/StreamcaseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eventmedia.streamcase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StreamcaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
